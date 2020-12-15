dbccad45cd52ae4d3067427772c349fd32051664
Kövér Ákos <akos.kover@stud.uni-courvinus.hu>
2020-12-15 21:16:09 +0100

    commit

--- IRF_Projekt_EH515M/IRF_Projekt_EH515M/Form1.Designer.cs
+++ IRF_Projekt_EH515M/IRF_Projekt_EH515M/Form1.Designer.cs
@@ -29,20 +29,117 @@ namespace IRF_Projekt_EH515M
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFutar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRendeles = new System.Windows.Forms.TextBox();
+            this.comboEtterem = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(711, 48);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(132, 43);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generálás";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(356, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(519, 332);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Futárazonosító:";
+            // 
+            // txtFutar
+            // 
+            this.txtFutar.Location = new System.Drawing.Point(155, 22);
+            this.txtFutar.Name = "txtFutar";
+            this.txtFutar.Size = new System.Drawing.Size(100, 22);
+            this.txtFutar.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rendelésazonosító:";
+            // 
+            // txtRendeles
+            // 
+            this.txtRendeles.Location = new System.Drawing.Point(177, 69);
+            this.txtRendeles.Name = "txtRendeles";
+            this.txtRendeles.Size = new System.Drawing.Size(100, 22);
+            this.txtRendeles.TabIndex = 5;
+            // 
+            // comboEtterem
+            // 
+            this.comboEtterem.FormattingEnabled = true;
+            this.comboEtterem.Location = new System.Drawing.Point(156, 130);
+            this.comboEtterem.Name = "comboEtterem";
+            this.comboEtterem.Size = new System.Drawing.Size(121, 24);
+            this.comboEtterem.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Étterem";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(913, 605);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboEtterem);
+            this.Controls.Add(this.txtRendeles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFutar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGenerate);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFutar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRendeles;
+        private System.Windows.Forms.ComboBox comboEtterem;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- IRF_Projekt_EH515M/IRF_Projekt_EH515M/Form1.cs
+++ IRF_Projekt_EH515M/IRF_Projekt_EH515M/Form1.cs
@@ -2,19 +2,91 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace IRF_Projekt_EH515M
 {
     public partial class Form1 : Form
     {
+        private Random random = new Random();
+        FutarszolgalatEntities context = new FutarszolgalatEntities();
         public Form1()
         {
             InitializeComponent();
+            context.Rendelés.Load();
+            context.Futár.Load();
+            context.Étterem.Load();
+            KeresesBetoltes();
+        }
+
+        private void KeresesBetoltes()
+        {
+            var kijelzo = from x in context.Rendelés
+                          select new
+                          {
+                              Rendelészám = x.RendelésID,
+                              Futárazonosító = x.FutárFK
+                          };
+            dataGridView1.DataSource = kijelzo.ToList();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            AddOrder();
+
+        }
+
+        private void AddOrder()
+        {
+            var étteremszám = (from s in context.Étterem select s).Count();
+
+            Rendelés ujrendeles = new Rendelés();
+            ujrendeles.Rögzítés = DateTime.Now;
+            ujrendeles.Elfogadva = ujrendeles.Rögzítés;
+            ujrendeles.Felvéve = ujrendeles.Rögzítés.AddSeconds(random.Next(1, 16));
+            ujrendeles.Leadva = ujrendeles.Felvéve.AddSeconds(random.Next(1, 16));
+            ujrendeles.Késés = random.Next(1, 16);
+            ujrendeles.Ár = random.Next(2000, 20000);
+            ujrendeles.Aktív = true;
+
+            try
+            {
+                var szabad = from x in context.Futár
+                             where x.Foglalt == false
+                             select x.FutárSK;
+                ujrendeles.FutárFK = szabad.First();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Nincs szabad futár a területen!");
+            }
+            ujrendeles.ÉtteremFK = random.Next(1, étteremszám + 1);
+
+            context.Rendelés.Add(ujrendeles);
+
+            context.SaveChanges();
+
+            // Query for the entity.
+            var futar = context.Futár.Single(e => e.FutárSK == ujrendeles.FutárFK);
+
+                // Entity is now tracked. Make a change to it.
+            futar.Foglalt = true;
+            context.SaveChanges();
+
+
+            KeresesBetoltes();
+
+            
+
+
+
         }
     }
 }
